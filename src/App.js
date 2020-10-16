f48098bf7ae80bd8e9805ec009782215a1d66ce3
Mustafa Can Yasa <mustafacan.yasa@dgpays.com>
2020-10-16 14:58:18 +0300

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,102 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+/* import { Container, Row, Col } from "react-bootstrap"; */
+class App extends React.Component {
+  state = {
+    items: [],
+    item: "",
+    addBtnClicked: false,
+    rdnmItm: "",
+  };
+  onItemChange = (e) => {
+    this.setState({
+      item: e.target.value,
+    });
+  };
+  addItem = (e) => {
+    e.preventDefault();
+    this.setState({
+      items: this.state.items.concat(this.state.item),
+      item: "",
+    });
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    if (this.state.items.length > 0) {
+      console.log("this.state.items.length", this.state.items.length);
+      this.setState({
+        addBtnClicked: true,
+      });
+    }
+  };
+  randomIt = (e) => {
+    const rItems = this.state.items;
+    console.log("rItems", rItems);
+
+    const randomItem = rItems[Math.floor(Math.random() * rItems.length)];
+    console.log("randomItems", randomItem);
+
+    this.setState({
+      rdnmItm: randomItem,
+    });
+  };
+  render() {
+    const submitDisabled = !this.state.item;
+    const addBtnClicked = !this.state.addBtnClicked;
+    return (
+      <div className="ui text container mt-5" id="app">
+        {" "}
+        <table className="ui selectable structured large table">
+          <thead>
+            <tr>
+              <th>Random Pick</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.items.map((item, idx) => (
+              <tr key={idx}>
+                <td>{item}</td>
+              </tr>
+            ))}
+          </tbody>
+          <tfoot>
+            <tr>
+              <th>
+                <form className="ui form" onSubmit={this.addItem}>
+                  <div className="field">
+                    <input
+                      className="prompt"
+                      type="text"
+                      placeholder="Add item..."
+                      value={this.state.item}
+                      onChange={this.onItemChange}
+                    />
+                  </div>
+                  <button
+                    className="ui button primary lg col-sm-12 mb-3"
+                    type="submit"
+                    disabled={submitDisabled}
+                  >
+                    Add item
+                  </button>
+                  <button
+                    className="ui button secondary lg col-sm-12 mb-3"
+                    type="button"
+                    disabled={addBtnClicked}
+                    onClick={(e) => this.randomIt(e, "value")}
+                  >
+                    RandomIt
+                  </button>
+                </form>
+              </th>
+            </tr>
+            <tr>
+              <td>Random Picked Item</td>
+            </tr>
+            <tr>
+              <td>{this.state.rdnmItm}</td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+    );
+  }
 }
-
 export default App;
